cc8937d37a9b120c1114ba656354880dae02e4bc
Jaime Pérez <88886098+AugustinCauchy@users.noreply.github.com>
2022-06-11 11:22:58 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -72,6 +72,13 @@ app.layout = html.Div([
             html.H2(
                 id='twitter-visit',
             )
+        ]), #AQUÍ METO MANO AL PROGRAMA
+        html.Div([
+            html.Img(src="https://logos-marcas.com/wp-content/uploads/2020/11/Twitch-Logo.png",
+                     style={"width": "100px"}),
+            html.H2(
+                id='twitch-visit',
+            )
         ]),
     ], style={"columnCount": 4, 'textAlign': "center"}),
     html.H3('Total Visits by Month', style={"textAlign": "center"}),
@@ -100,6 +107,8 @@ app.layout = html.Div([
     Output('facebook-visit', 'children'),
     Output('instagram-visit', 'children'),
     Output('twitter-visit', 'children'),
+    #AQUÍ METO MANO TAMBIÉN
+    Output('twitch-visit', 'children'),
     Output('total-visit-line', 'figure'),
     Output('total-visit-social-networks-line', 'figure'),
     Output('world-map', 'figure'),
@@ -144,7 +153,16 @@ def update_figures(start_date_selected, end_date_selected, social_networks_selec
              (df.datetime >= start_date_selected) &
              (df.datetime <= end_date_selected)]
     ).shape[0]
-
+################################################################################################
+        twitch_visit = (
+        df
+        .loc[(df.social_network == 'twitch') &
+             (df.social_network.isin(social_networks_selected)) &
+             (df.device.isin(devices_selected)) &
+             (df.datetime >= start_date_selected) &
+             (df.datetime <= end_date_selected)]
+    ).shape[0]
+################################################################################################
     df_by_month = (
         df
         .loc[(df.social_network.isin(social_networks_selected)) &
@@ -237,9 +255,9 @@ def update_figures(start_date_selected, end_date_selected, social_networks_selec
             'name': 'Total Visits'
         }
     )
-
-    return total_visit, facebook_visit, instagram_visit, twitter_visit, total_visit_fig, total_visit_social_network_fig, world_map_fig, devices_pie_fig
-
+#################################################################################################################################################################################
+    return total_visit, facebook_visit, instagram_visit, twitter_visit, twitch_visit, total_visit_fig, total_visit_social_network_fig, world_map_fig, devices_pie_fig
+##########################################################################################################################################################################################
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', port="80")
